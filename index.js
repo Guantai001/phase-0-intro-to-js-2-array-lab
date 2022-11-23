574b4840db4de6900382622917c3ed5383c49df9
guantai <jpguantai@gmail.com>
2022-11-23 11:19:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,44 +2,50 @@
 
 var cats = ["Milo", "Otis", "Garfield"]
 
-function  destructivelyAppendCat(name){
-
-    cats.push(name);
-    return cats;
-}
-
-function  destructivelyPrependCat(name){
-    cats.unshift(name);
+function destructivelyAppendCat(name){
+    cats.push(name)
+    
     return cats
 }
 
-function  destructivelyRemoveLastCat(){
-    cats.pop();
-    return cats;
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
 }
 
-function  destructivelyRemoveFirstCat(){
-    cats.shift();
-    return cats;
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
 }
 
 function appendCat(name){
-    var myCat=[ ...cats,name];
-    return myCat
+    var newCats = [...cats, name]
+    return newCats
 }
 
-function  prependCat(name){
-    var hisCat = [name,...cats];
-    return hisCat
+function prependCat(name){
+    var newCats = [name, ...cats]
+    return newCats
+
 }
 
-function   removeLastCat(){
-    var kat = cats.slice(0,cats.length-1);
- 
-   return kat;
+function removeLastCat(){
+    var newCats = cats.slice(0, cats.length -1)
+    return newCats
 }
 
-function  removeFirstCat(){
-    var paka = cats.slice(1)
-    return paka
+function removeFirstCats(){
+    var newCats = cats.slice(1)
+    return newCats
 }
+
+function removeFirstCat(){
+    var newCats = cats.slice(1)
+    return newCats
+}
+// console.log(cats)
